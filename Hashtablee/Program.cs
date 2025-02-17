963e014b20cd1607b9b4508c8a7b4bf535f6335d
Jayanthkumar04 <jayanthkumar598@gmail.com>
2025-02-17 16:06:07 +0530

    QUEUE AND STACK ARE DONEE

--- Hashtablee/Program.cs
+++ Hashtablee/Program.cs
@@ -101,6 +101,8 @@ namespace Hashtablee
             }
 
 
+
+
             Console.ReadKey();
         }
     }
---
+++ StacksAndOthers/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndOthers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************STACKSSS************************");
+
+            Stack stack = new Stack();
+
+            stack.Push("jayanht");
+
+            stack.Push("ram");
+
+            Console.WriteLine("stored values \t"+stack.Count);
+
+            foreach(var  item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("pop result :\t " + stack.Pop());
+
+
+            Console.WriteLine("peek result: \t"+stack.Peek());
+            Console.WriteLine("stored values :after peek\t"+stack.Count);
+
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("peek result:\t"+stack.Peek());
+
+            Console.WriteLine("stored value after peek \t"+stack.Count);
+
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("**************QUEUES**********************");
+
+            Queue queue = new Queue();
+
+            queue.Enqueue ("jayanht");
+
+            queue.Enqueue("ram");
+
+            queue.Enqueue("zaheer");
+
+            Console.WriteLine("stored values \t" + queue.Count);
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Dequeue result :\t " + queue.Dequeue());
+
+
+            Console.WriteLine("peek result: \t" + queue.Peek());
+            Console.WriteLine("stored values :after peek\t" + queue.Count);
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("peek result:\t" + queue.Peek());
+
+            Console.WriteLine("stored value after peek \t" + queue.Count);
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+ 
+        }
+    }
+}
