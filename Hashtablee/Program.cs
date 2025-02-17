b7d3abf64b6a959706cf41024572463bcb584a9f
Jayanthkumar04 <jayanthkumar598@gmail.com>
2025-02-17 15:49:38 +0530

    SORTED LIST DONE

--- Hashtablee/Program.cs
+++ Hashtablee/Program.cs
@@ -69,6 +69,38 @@ namespace Hashtablee
                 Console.WriteLine(k + "\t\t" + ht2[k]);
             }
 
+
+            Console.WriteLine("SORTED LIST");
+            SortedList st = new SortedList()
+            {
+                { 1,"harddisk" },
+                {2,"random access memory" },
+                {6,"compact disk" },
+                {3,"kilo byte" }
+
+            };
+
+            Console.WriteLine("stored valued count \t" + st.Count);
+
+            Console.WriteLine("keys");
+            foreach (var k in st.Keys)
+            {
+                Console.WriteLine(k);
+            }
+
+            Console.WriteLine("values");
+            foreach (var k in st.Values)
+            {
+                Console.WriteLine(k);
+            }
+
+            Console.WriteLine("key \t value");
+            foreach (var k in st.Keys)
+            {
+                Console.WriteLine(k + "\t\t" + st[k]);
+            }
+
+
             Console.ReadKey();
         }
     }
